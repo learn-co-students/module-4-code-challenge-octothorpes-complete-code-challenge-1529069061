86f746d9b1e76a1cebb5bdb6789908e56a407b03
github username <github email address>
2018-06-15 10:30:55 -0400

    finished regular deliverables

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -5,11 +5,15 @@ class BotCollection extends React.Component {
   //your code here
 
   render(){
+    //console.log(this.props)
+    //if (this.props) {
+      const bots = this.props.botCollection.map(
+        bot => <BotCard key={bot.id} bot={bot} addBot={this.props.addBot} />)
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {bots}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,51 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    bots: [],
+    yourBots: [],
+  }
+
+  componentDidMount() {
+    fetch(`https://bot-battler-api.herokuapp.com/api/v1/bots`).then(r => r.json())
+    .then(bots => this.setState({ bots}))
+  }
+
+  addBot = (id) => {
+    let yourBot = this.state.bots.find(bot => bot.id === id);
+    const added = true;
+    yourBot = {...yourBot, added};
+    //console.log(yourBot);
+    const alreadyOwn = this.state.yourBots.find(bot => bot.id === yourBot.id);
+
+    if (!alreadyOwn) {
+      this.setState({
+        yourBots: [...this.state.yourBots, yourBot]
+      })
+    }
+  }
+
+  removeBot = (id) => {
+    let yourBot = this.state.yourBots.find(bot => bot.id === id);
+
+    const yourBots = [
+      ...this.state.yourBots.slice(0, this.state.yourBots.indexOf(yourBot)),
+      ...this.state.yourBots.slice(this.state.yourBots.indexOf(yourBot)+1)
+    ]
+
+    this.setState({
+      yourBots
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy yourBots={this.state.yourBots} removeBot={this.removeBot}/>
+        <BotCollection botCollection={this.state.bots} addBot={this.addBot} />
       </div>
     );
   }
