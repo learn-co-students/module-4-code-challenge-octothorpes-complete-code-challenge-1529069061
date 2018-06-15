af146dd19fcbd5786c20d8773136e7b1f45cc49c
Daniel Brazel <dbrazel4@gmail.com>
2018-06-15 11:00:03 -0400

    My best try

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,13 +2,32 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+  constructor(props){
+    super(props)
+
+    this.state={
+      bots: props.bots
+    }
+  }
+
+
+  showBots = () => {
+    return this.props.bots.map( bot =>{
+    return  <BotCard key={bot.id} bot={bot} clickHandler={this.props.clickHandler} />
+    })
+  }
+
+  componentDidMount(){
+    this.showBots()
+  }
+
 
   render(){
+    console.log("collection", this.props)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.showBots()}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,36 @@
 import React from "react";
-
+import BotCollection from './BotCollection.js';
+import YourBotArmy from './YourBotArmy.js'
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor(){
+    super()
+    this.state = {
+      bots: []
+    }
+  }
 
+  clickHandler = (e) => {
+    e.stopPropagation()
+    console.log(e.target)
+  }
+//I can't figure out how how to get
+//the bot object from the click event
+// the <div className="ui card" can't be clicked directly.
+
+  fetcher = () => fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+  .then(res => res.json())
+  .then(json => this.setState({bots: json}))
+
+
+  componentDidMount(){
+    this.fetcher()
+  }
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy />
+        <BotCollection bots={this.state.bots} clickHandler={this.clickHandler} />
       </div>
     );
   }
